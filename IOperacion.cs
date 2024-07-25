7c77d0c857387dc4b03e32a79f4d2984e4dca89b
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-07-24 20:55:51 -0600

    Create IOperacion.cs

---
+++ IOperacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaz
+{
+    /// <summary>
+    /// interfaz que contiene metodos para calcular operaciones matematicas basicas.
+    /// </summary>
+   public interface IOperacion
+    {
+
+    }
+}
